f727f9fe1c5660149a2dab4b554314be6b0f217b
Emirhan <emirhanakdeeniz@gmail.com>
2025-07-08 14:21:18 +0300

    backend fixes

--- api/products.js
+++ api/products.js
@@ -1,5 +1,10 @@
-import products from "../products.json";
+/* eslint-disable no-undef */
+import fs from "fs";
+import path from "path";
 
 export default function handler(req, res) {
+  const filePath = path.resolve(process.cwd(), "products.json");
+  const fileData = fs.readFileSync(filePath, "utf-8");
+  const products = JSON.parse(fileData);
   res.status(200).json(products);
 }
--- api/products/filter.js
+++ api/products/filter.js
@@ -1,22 +1,24 @@
-import products from "../../products.json";
+/* eslint-disable no-undef */
+
+import fs from "fs";
+import path from "path";
 
 export default function handler(req, res) {
-  let filteredProducts = products;
+  const filePath = path.resolve(process.cwd(), "products.json");
+  const fileData = fs.readFileSync(filePath, "utf-8");
+  let products = JSON.parse(fileData);
+
   const { color, minPrice, maxPrice } = req.query;
 
   if (color) {
-    filteredProducts = filteredProducts.filter((p) => p.colors.includes(color));
+    products = products.filter((p) => p.colors.includes(color));
   }
   if (minPrice && !isNaN(parseFloat(minPrice))) {
-    filteredProducts = filteredProducts.filter(
-      (p) => p.price >= parseFloat(minPrice)
-    );
+    products = products.filter((p) => p.price >= parseFloat(minPrice));
   }
   if (maxPrice && !isNaN(parseFloat(maxPrice))) {
-    filteredProducts = filteredProducts.filter(
-      (p) => p.price <= parseFloat(maxPrice)
-    );
+    products = products.filter((p) => p.price <= parseFloat(maxPrice));
   }
 
-  res.status(200).json(filteredProducts);
+  res.status(200).json(products);
 }
